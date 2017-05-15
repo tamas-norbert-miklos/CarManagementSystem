e38b74c0e08d5e096256136447baf425c489a3c8
tamas-norbert-miklos <miklos.tamas.norbert@gmail.com>
2017-05-15 12:40:21 +0300

    Unit tests added.

--- src/main/java/ro/fortech/dao/inmemory/IMCarDAO.java
+++ src/main/java/ro/fortech/dao/inmemory/IMCarDAO.java
@@ -2,8 +2,6 @@ package ro.fortech.dao.inmemory;
 
 import ro.fortech.dao.CarDAO;
 import ro.fortech.model.Car;
-import ro.fortech.model.EngineType;
-import ro.fortech.model.TransmissionType;
 
 import java.util.*;
 
@@ -13,54 +11,54 @@ import java.util.*;
 //@Component
 public class IMCarDAO extends IMBaseDAO<Car> implements CarDAO {
     public IMCarDAO() {
-        Car car = new Car();
-        car.setManufacturer("Audi");
-        car.setSellerId(1);
-        car.setType("A5");
-        car.setFabricationYear(2005);
-        car.setMileAge(12000);
-        car.setPrice(5000);
-        car.setEngineType(EngineType.DIESEL);
-        car.setTransmissionType(TransmissionType.MANUAL);
-        car.setColour("Blue");
-        car.setExtras("Some extras");
-        car.setMatriculated(true);
-        car.setImgUrl("1494507751809_audi-a5.jpg");
-
-        Car car2 = new Car();
-        car2.setManufacturer("Audi");
-        car2.setSellerId(1);
-        car2.setType("A4");
-        car2.setFabricationYear(2010);
-        car2.setMileAge(50000);
-        car2.setPrice(6000);
-        car2.setEngineType(EngineType.PETROL);
-        car2.setTransmissionType(TransmissionType.AUTOMATIC);
-        car2.setColour("Blue");
-        car2.setExtras("Some more extras");
-        car2.setMatriculated(true);
-        car2.setImgUrl("1494507766547_audi-a4.jpg");
-
-        Car car3 = new Car();
-        car3.setManufacturer("Ferrari");
-        car3.setSellerId(2);
-        car3.setType("458 Italia");
-        car3.setFabricationYear(2007);
-        car3.setMileAge(50000);
-        car3.setPrice(6000);
-        car3.setEngineType(EngineType.PETROL);
-        car3.setTransmissionType(TransmissionType.AUTOMATIC);
-        car3.setColour("Red");
-        car3.setExtras("Some more extras");
-        car3.setMatriculated(true);
-        car3.setImgUrl("1494507632680_ferrari458.jpg");
-
-        update(car);
-        update(car2);
-        update(car3);
-        System.out.println(car.getId());
-        System.out.println(car2.getId());
-        System.out.println(car3.getId());
+//        Car car = new Car();
+//        car.setManufacturer("Audi");
+//        car.setSellerId(1);
+//        car.setType("A5");
+//        car.setFabricationYear(2005);
+//        car.setMileAge(12000);
+//        car.setPrice(5000);
+//        car.setEngineType(EngineType.DIESEL);
+//        car.setTransmissionType(TransmissionType.MANUAL);
+//        car.setColour("Blue");
+//        car.setExtras("Some extras");
+//        car.setMatriculated(true);
+//        car.setImgUrl("1494507751809_audi-a5.jpg");
+//
+//        Car car2 = new Car();
+//        car2.setManufacturer("Audi");
+//        car2.setSellerId(1);
+//        car2.setType("A4");
+//        car2.setFabricationYear(2010);
+//        car2.setMileAge(50000);
+//        car2.setPrice(6000);
+//        car2.setEngineType(EngineType.PETROL);
+//        car2.setTransmissionType(TransmissionType.AUTOMATIC);
+//        car2.setColour("Blue");
+//        car2.setExtras("Some more extras");
+//        car2.setMatriculated(true);
+//        car2.setImgUrl("1494507766547_audi-a4.jpg");
+//
+//        Car car3 = new Car();
+//        car3.setManufacturer("Ferrari");
+//        car3.setSellerId(2);
+//        car3.setType("458 Italia");
+//        car3.setFabricationYear(2007);
+//        car3.setMileAge(50000);
+//        car3.setPrice(6000);
+//        car3.setEngineType(EngineType.PETROL);
+//        car3.setTransmissionType(TransmissionType.AUTOMATIC);
+//        car3.setColour("Red");
+//        car3.setExtras("Some more extras");
+//        car3.setMatriculated(true);
+//        car3.setImgUrl("1494507632680_ferrari458.jpg");
+//
+//        update(car);
+//        update(car2);
+//        update(car3);
+//        System.out.println(car.getId());
+//        System.out.println(car2.getId());
+//        System.out.println(car3.getId());
     }
 
     @Override
---
+++ src/test/java/ro/fortech/CarServiceTest.java
@@ -0,0 +1,23 @@
+package ro.fortech;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ro.fortech.service.CarService;
+
+/**
+ * Created by Tamas on 4/22/2017.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:beans.xml"})
+public class CarServiceTest extends BaseCarServiceTest {
+
+    @Autowired
+    private CarService carService;
+
+    @Override
+    protected CarService getCarService() {
+        return carService;
+    }
+}
